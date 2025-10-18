3709bc6f9a6bce3a64bdba0ee46d66e2a6980cd4
Nikhil NM <nikhimanjunath@gmail.com>
2025-10-18 15:03:09 +0530

    Update index.js

--- Server/server/index.js
+++ Server/server/index.js
@@ -25,7 +25,7 @@ app.use('/api/employees', employeeRoutes);
 app.use('/api/attendance', attendanceRoutes);
 
 // Server + DB
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => {

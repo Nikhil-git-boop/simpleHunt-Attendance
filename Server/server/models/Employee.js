4adbe51d60f122b5c60f350548fdbe64a62bac47
Nikhil NM <nikhimanjunath@gmail.com>
2025-10-23 10:29:47 +0530

    Update Employee.js

--- Server/server/models/Employee.js
+++ Server/server/models/Employee.js
@@ -1,9 +1,25 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
+const bcrypt = require('bcryptjs');
 
-const employeeSchema = new Schema({
+const employeeSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  createdBy: { type: Schema.Types.ObjectId, ref: 'User' }
-}, { timestamps: true });
+  employeeId: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  department: { type: String, required: true },
+  password: { type: String, required: true }
+});
+
+// Hash password before saving
+employeeSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Compare password method
+employeeSchema.methods.comparePassword = async function (password) {
+  return bcrypt.compare(password, this.password);
+};
 
 module.exports = mongoose.model('Employee', employeeSchema);
